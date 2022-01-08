9a4265f523268d442f8e9dca09e49d0386ba0877
Kim sm <dev.sonohoshi@gmail.com>
2022-01-09 03:17:18 +0900

    Add test code about mouse input

---
+++ Assets/Scripts/DragController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public class DragController : MonoBehaviour
+{
+    private Vector2 _startPosition;
+    private Camera _mainCam;
+    
+    [SerializeField] private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        _mainCam = Camera.main;
+    }
+
+    public bool GetInput(out float distance)
+    {
+        distance = 0;
+        if (Input.GetMouseButtonDown(0))
+        {
+            _startPosition = _mainCam.ScreenToWorldPoint(Input.mousePosition);
+            lineRenderer.positionCount = 2;
+            lineRenderer.SetPositions(new[] {(Vector3) _startPosition, (Vector3) _startPosition});
+            return false;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            lineRenderer.SetPosition(1, (Vector2) _mainCam.ScreenToWorldPoint(Input.mousePosition));
+            return false;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            distance = (_startPosition - (Vector2) _mainCam.ScreenToWorldPoint(Input.mousePosition)).magnitude;
+            _startPosition = Vector3.zero;
+            lineRenderer.positionCount = 0;
+            return true;
+        }
+
+        return false;
+    } 
+}
---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private DragController controller;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (controller.GetInput(out var distance))
+        {
+            Debug.Log($"distance : {distance}");
+            _rigidbody.AddForce(Vector2.up * distance, ForceMode2D.Impulse);
+        }
+    }
+}
