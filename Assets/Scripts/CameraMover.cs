ae297b10c540503229e1ec76e21e32ff8018a99e
Kim sm <dev.sonohoshi@gmail.com>
2022-01-12 10:37:16 +0900

    엄청나게 급했던 마무리

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private bool _endOpening;
+
+    private void Start()
+    {
+        transform.DOMoveY(4, 3f).SetEase(Ease.InCirc).OnComplete(() => _endOpening = true);
+    }
+
+    private void LateUpdate()
+    {
+
+        if (_endOpening)
+        {
+            var pos = transform.position;
+            if (target.position.y >= 4)
+            {
+                pos.y = target.position.y;
+                transform.position = pos;
+            }
+        }
+    }
+}
